16d98016d0c4b2864a03f99929b1e41364208f5f
Sidich-S <106704084+Sidich-S@users.noreply.github.com>
2022-10-03 20:45:59 +0300

    AdD

---
+++ src/src/js/task-01.js
@@ -0,0 +1,15 @@
+
+// const categoryRef = document.querySelectorAll(`ul#categories`);
+
+// const ulRef = document.querySelectorAll(`li.item`);
+// console.log(`Number of categories:`, ulRef.length);
+
+
+// const nameCategRef = document.querySelectorAll(`.item`);
+
+// nameCategRef.forEach( element => {
+//     const categName = element.firstElementChild;
+//     const elemNumber = element.querySelectorAll("li");
+//     console.log(`Category: ${categName.textContent}`);
+//     console.log(`Elements: ${elemNumber.length}`)
+// });
--- src/src/js/task-02.js
+++ src/src/js/task-02.js
@@ -1,8 +1,31 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+// const ingredients = [
+//   'Potatoes',
+//   'Mushrooms',
+//   'Garlic',
+//   'Tomatos',
+//   'Herbs',
+//   'Condiments',
+// ];
+
+// const ingredientsRef = document.querySelector(`ul`);
+
+// function makeIngredients(ingredients) {
+//   return ingredients.map((text) => {
+//     const liRef = document.createElement(`li`);
+//     liRef.textContent = text;
+//     liRef.classList.add(`item`);
+//     console.log(liRef);
+//     return liRef
+//   });
+// };
+
+// const elemLi = makeIngredients(ingredients);
+// ingredientsRef.append(...elemLi);
+
+
+// Напиши скрипт, который для каждого элемента массива ingredients:
+
+// Создаст отдельный элемент <li>. Обзательно используй метод document.createElement().
+// Добавит название ингредиента как его текстовое содержимое.
+// Добавит элементу класс item.
+// После чего вставит все <li> за одну операцию в список ul#ingredients.
