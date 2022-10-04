b0367f4a29409b80d9ff5a6b98b5837eaf8c3b8b
Sidich-S <106704084+Sidich-S@users.noreply.github.com>
2022-10-04 18:06:14 +0300

    Add7-9

---
+++ src/src/js/task-07.js
@@ -0,0 +1,13 @@
+// Напиши скрипт, который реагирует на изменение значения input#font - size - control(событие input)
+// и изменяет инлайн - стиль span#text обновляя свойство font - size.В результате при перетаскивании
+// ползунка будет меняться размер текста.
+
+const simplyBtn = document.querySelector("#font-size-control");
+const wordRef = document.querySelector('#text');
+
+simplyBtn.addEventListener('change', onTextChangeFont)
+
+
+function onTextChangeFont() {
+    wordRef.style.fontSize = `${simplyBtn.value}px`
+    }
--- src/src/js/task-09.js
+++ src/src/js/task-09.js
@@ -1,3 +1,19 @@
+// Напиши скрипт, который изменяет цвета фона элемента <body> через инлайн стиль при клике на button.change-color и выводит значение цвета в span.color.
+
+// Для генерации случайного цвета используй функцию getRandomHexColor.
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const nextColorRef = document.querySelector(`.change-color`);
+const colorRef = document.querySelector(`.color`);
+const body = document.querySelector(`body`);
+
+nextColorRef.addEventListener(`click`, (event) => {
+  const randomColorRef = getRandomHexColor();
+  body.style.backgroundColor = randomColorRef;
+  colorRef.textContent = randomColorRef;
+});
